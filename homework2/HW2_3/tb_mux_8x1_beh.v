2581cf0834a9c572c57916ca93e1078a62d485fe
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-13 22:32:10 +0800

    Create tb_mux_8x1_beh.v

---
+++ homework2/HW2_3/tb_mux_8x1_beh.v
@@ -0,0 +1,20 @@
+module tb_mux_8x1_beh;
+	reg x,y,z;
+	wire S,C;
+	
+	Full_adder uut(x,y,z,S,C);
+	
+	initial 
+		begin
+			#10 x=1'b0;
+				y=1'b0;
+				z=1'b0;
+			#10 x=1'b1;
+				y=1'b0;
+				z=1'b1;
+			#10 x=1'b1;
+				y=1'b1;
+				z=1'b1;
+		end
+		initial #40 $finish;
+endmodule
