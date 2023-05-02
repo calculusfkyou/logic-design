619bfcdfe924d9bbec9c491e2d0c145026540113
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-05-02 15:18:40 +0800

    Create Toggle_flip_flop.v

---
+++ lab3-3/Toggle_flip_flop.v
@@ -0,0 +1,38 @@
+// Description of Toggle (T) flip-flop
+module D_flip_flop_AR (Q, D, Clk, rst);
+  output 	Q;
+  input	D, Clk, rst;
+  reg 	Q;
+
+  always @ (posedge Clk, negedge rst)	
+    if (rst == 0) Q <= 1'b0;
+    else Q <= D;	 
+endmodule
+
+module Toggle_flip_flop_1 (Q, T, Clk, rst);
+  output 	Q;
+  input	T, Clk,rst;
+  reg 	Q;
+
+  always @ (posedge Clk, negedge rst)	
+    if (!rst) Q <= 1'b0;
+    else if (T) Q <= !Q;	 
+endmodule
+
+module Toggle_flip_flop_2 (Q, T, Clk, rst);
+  output 	Q;
+  input	T, Clk, rst;
+  wire	DT;
+  assign DT = T ^ Q;
+  D_flip_flop_AR M0 (Q, DT, Clk, rst);
+endmodule
+
+module Toggle_flip_flop_3 (Q, T, Clk, rst);
+  output 	Q;
+  input	T, Clk, rst;
+  reg 	Q;
+
+  always @ (posedge Clk, negedge rst)	
+    if (!rst) Q <= 1'b0;
+    else  Q <= Q ^ T;	 
+endmodule
