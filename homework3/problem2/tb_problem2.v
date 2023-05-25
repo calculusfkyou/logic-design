3b2715e1a0c9e5c65075f7fde5c327a4f17218b7
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-05-26 03:02:55 +0800

    Create tb_problem2.v

---
+++ homework3/problem2/tb_problem2.v
@@ -0,0 +1,21 @@
+module tb_problem2;
+  wire y_out;
+  reg x_in, clock, reset;
+  problem2 uut(.y_out(y_out),.x_in(x_in),.clock(clock),.reset(reset));
+  initial #90 $finish;
+  always #5 clock=~clock;
+  initial
+	begin
+		#0 clock=0;
+		   reset=0;
+		#10 reset=1;
+			x_in=0;
+		#10 x_in=1;	
+		#10 x_in=1;	
+		#10 x_in=1;	
+		#10 x_in=0;	
+		#10 x_in=1;	
+		#10 x_in=0;	
+		#10 x_in=0;	
+	end
+endmodule
