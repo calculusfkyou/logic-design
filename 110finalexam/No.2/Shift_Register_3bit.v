c84a8ae4d85c4d1975c80ddbe4e0a6bd4f9f67c0
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-06-08 01:32:17 +0800

    Create Shift_Register_3bit.v

---
+++ 110finalexam/No.2/Shift_Register_3bit.v
@@ -0,0 +1,15 @@
+module Shift_Register_3bit(
+	output reg [3:0]A_par,
+	input [3:0]I_par,
+	input s1,s0,MSB_in,LSB_in,CLK,Clear_b
+	);
+	always@(posedge CLK,negedge Clear_b)
+		if(~Clear_b) A_par<=0;
+		else
+			case({s1,s0})
+				2'b00:A_par<=A_par;
+				2'b01:A_par<={MSB_in,A_par[3:1]};
+				2'b10:A_par<={A_par[2:0],LSB_in};
+				2'b11:A_par<=I_par;
+			endcase
+endmodule
