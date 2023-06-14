5595b6544726411c6e8ad9f99025205cdb8dedca
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-06-15 00:16:13 +0800

    Update detector.v

--- 110finalexam/No.3/detector.v
+++ 110finalexam/No.3/detector.v
@@ -3,7 +3,7 @@ module detector(
 	input x_in,clock,reset
 );
 	reg [2:0]state;
-	parameter S0=3'b000,S1=3'b001,S2=3'b010,S3=3'b011,S4=3'b100,S5=3'b101,S6=3'b110;
+	parameter S0=3'b000,S1=3'b001,S2=3'b010,S3=3'b011,S4=3'b100,S5=3'b101;
 	always@(posedge clock,negedge reset)
 		if(reset==0) state<=S0;
 		else 
@@ -13,8 +13,7 @@ module detector(
 				S2:if(x_in) state<=S3;
 				S3:if(x_in) state<=S4;
 				S4:if(x_in) state<=S5;
-				S5:if(x_in) state<=S6;
-				S6:if(x_in) state<=S0;
+				S5:if(x_in) state<=S0;
 			endcase
-	assign y_out=((state==S2)||(state==S3)||(state==S4));
+	assign y_out=(state==S2)||(state==S3);
 endmodule
